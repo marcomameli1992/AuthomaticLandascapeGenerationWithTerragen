a3b4d8dea8620136531bc626beed69b37c6bf203
marcomameli1992 <m.mameli@pm.univpm.it>
2021-10-05 15:25:35 +0200

    Added the string formatting to reach the right element

--- basicComponent/terrain.py
+++ basicComponent/terrain.py
@@ -3,6 +3,6 @@ import xml.etree.ElementTree as ET
 # TODO define the range for all the parameters
 
 def change_basic_terrain(tags_root: ET.Element, attribute='Strata'):
-    tag = tags_root.find(".//*[@name='Strata']")
+    tag = tags_root.find(f".//*[@name='{attribute}']")
     tag.attrib['name'] = "StrataChanged" # changing the name It is usefull only to have a note of teh method to access to an attribute
     return tags_root

c474680d4fcd12ccbea95dc3bb09c697aafc7aa6
marcomameli1992 <m.mameli@pm.univpm.it>
2021-10-05 15:23:25 +0200

    Adding the TODO

--- basicComponent/terrain.py
+++ basicComponent/terrain.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 
+# TODO define the range for all the parameters
+
 def change_basic_terrain(tags_root: ET.Element, attribute='Strata'):
     tag = tags_root.find(".//*[@name='Strata']")
-    tag.attrib['name'] = "StrataChanged"
+    tag.attrib['name'] = "StrataChanged" # changing the name It is usefull only to have a note of teh method to access to an attribute
     return tags_root

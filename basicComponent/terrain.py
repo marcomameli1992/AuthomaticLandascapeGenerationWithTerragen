2bd3bc5dfcd9701c96de7336a2037403faa50f47
marcomameli1992 <m.mameli@pm.univpm.it>
2021-10-05 16:03:35 +0200

    added the basic documentation structure

--- basicComponent/terrain.py
+++ basicComponent/terrain.py
@@ -2,7 +2,13 @@ import xml.etree.ElementTree as ET
 
 # TODO define the range for all the parameters
 
-def change_basic_terrain(tags_root: ET.Element, attribute='Strata'):
+def change_basic_terrain(tags_root: ET.Element, attribute='BasicTerrain'):
+    '''
+
+    :param tags_root:
+    :param attribute:
+    :return:
+    '''
     tag = tags_root.find(f".//*[@name='{attribute}']")
     tag.attrib['name'] = "StrataChanged" # changing the name It is usefull only to have a note of teh method to access to an attribute
     return tags_root

2f55823e795949e476e5b39dceda3b1c5c87400f
marcomameli1992 <m.mameli@pm.univpm.it>
2021-10-05 15:21:37 +0200

    Testing the first function for terrain changes
    This function is used in the final coda. 
    The test consist if it can be used a xpath search for the name attribute of the tag to find it

--- PieceTesting/tagListExtraction.py
+++ PieceTesting/tagListExtraction.py
@@ -2,10 +2,16 @@
 import xml.etree.ElementTree as ET
 import os
 
+def change_basic_terrain(tags_root: ET.Element, attribute='Strata'):
+    tag = tags_root.find(".//*[@name='Strata']")
+    tag.attrib['name'] = "StrataChanged"
+    return tags_root
+
 # opening the file
 file_path = os.path.join('..', 'TerragenOriginalFile', 'Base.tgd')
 tree = ET.parse(file_path)
+root = tree.getroot()
+# Terrain extraction
+tag = root.find(".//*[@name='Strata']") # Search query for the XML by the name in the TAG
 
-for element in tree.iter():
-    with open(os.path.join('..', 'basicTag', element.tag + '.xml'), 'wb') as element_file:
-        
+changed = change_basic_terrain(root)

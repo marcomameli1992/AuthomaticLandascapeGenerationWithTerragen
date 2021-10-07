458b98038d7626ebcffeb76804be720c0f921acf
marcomameli1992 <m.mameli@pm.univpm.it>
2021-10-07 17:50:25 +0200

    Added range defintion

--- basicComponent/light.py
+++ basicComponent/light.py
@@ -1,23 +1,32 @@
 import xml.etree.ElementTree as ET
+import random
+import os
+import json
+import numpy as np
+
+range_value_path = os.path.join('..', 'basicComponent', 'ranges', 'light.json')
 
 def change_sunllight(tags_root: ET.Element, attribute="Sun") -> ET.Element:
+    with open(range_value_path, 'r') as range_file:
+        ranges = json.load(range_file)
+    sunlight_ranges = ranges['sunlight']
     tag = tags_root.find(f".//*[@name='{attribute}']")
-    tag.attrib['enable'] = "0"
-    tag.attrib['colour'] = '1 5 0'
-    tag.attrib['light_surfaces'] = '0'  # or 1
-    tag.attrib['light_atmosphere'] = '0'  # or 1
-    tag.attrib['heading'] = '4' # degrees 0 for North 90 for Est
-    tag.attrib['elevation'] = '0' # 0 is the horizon and 90 is the overhead
-    tag.attrib['strength'] = '1'  # the power of the sunlight maximum is 5 ( better integer value step)
+    tag.attrib['enable'] = "1" # the sunlight has to be active otherwise the render is dark
+    #tag.attrib['colour'] = '1 5 0'
+    tag.attrib['light_surfaces'] = '1'  # or 1
+    tag.attrib['light_atmosphere'] = '1'  # or 1
+    tag.attrib['heading'] = str(random.uniform(sunlight_ranges['heading_minimum'], sunlight_ranges['heading_maximum'])) # degrees 0 for North 90 for Est
+    tag.attrib['elevation'] = str(random.uniform(sunlight_ranges['elevation_minimum'], sunlight_ranges['elevation_maximum'])) # 0 is the horizon and 90 is the overhead
+    tag.attrib['strength'] = str(random.uniform(sunlight_ranges['strenght_minimum'], sunlight_ranges['strenght_maximum']))  # the power of the sunlight maximum is 5 ( better integer value step)
     tag.attrib['cast_shadow'] = '1' # or 0 to activate or not
     tag.attrib['shadow_onSurfaces'] = '1' # or 0 to activate or not
     tag.attrib['shadow_of_atmosphere'] = '1'
     tag.attrib['soft_shadow'] = '1'
-    tag.attrib['soft_shadow_diameter'] = '0'  # degrees
+    tag.attrib['soft_shadow_diameter'] = '0'  # TBD degrees
     tag.attrib['soft_shadow_samples'] = '4'
     tag.attrib['soft_shadow_samples_jitter'] = '0'
     tag.attrib['glow_in_atmosphere'] = '1'
-    tag.attrib['specular_highlights'] = '1' # or 0 to activate or not
+    tag.attrib['specular_highlights'] = '0' # or 0 to activate or not
     tag.attrib['visible_disc'] = '1' # or 0 to activate or not
     tag.attrib['angular_diameter'] = '1' # the size of the disk
     return tags_root

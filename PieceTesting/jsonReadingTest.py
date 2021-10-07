31194162ce48e5c95de9339c961fc8bc0ec120f8
marcomameli1992 <m.mameli@pm.univpm.it>
2021-10-07 15:52:21 +0200

    Tested the function that generate a string of 3 integer value for positioning

--- PieceTesting/jsonReadingTest.py
+++ PieceTesting/jsonReadingTest.py
@@ -1,6 +1,7 @@
 import os
 import json
 import random
+import numpy as np
 
 range_value_path = os.path.join('..', 'basicComponent', 'ranges', 'terrain.json')
 
@@ -10,3 +11,5 @@ with open(range_value_path, 'r') as range_file:
 print(ranges['fractal_terrain']['enable'])
 
 print(random.uniform(ranges['fractal_terrain']['feature_scale_minimum'], ranges['fractal_terrain']['feature_scale_maximum']))
+
+print(' '.join(map(str, list(np.random.randint(low=0, high=1e+7, size=3)))))

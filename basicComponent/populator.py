eb1911fc805f482c16215e30febf25ba5a517f6e
marcomameli1992 <m.mameli@pm.univpm.it>
2021-10-14 16:17:19 +0200

    Created the functions for the changes of populator in Terragen

---
+++ basicComponent/populator.py
@@ -0,0 +1,55 @@
+import xml.etree.ElementTree as ET
+import random
+import os
+import json
+import numpy as np
+
+range_value_path = os.path.join('..', 'basicComponent', 'ranges', 'terrain.json') # TODO changes the path
+
+def change_trees_population(tags_root: ET.Element, attribute="TreesOBJ") -> ET.Element:
+    with open(range_value_path, 'r') as range_file:
+        ranges = json.load(range_file)
+    populator_ranges = ranges['trees']
+
+    tag = tags_root.find(f".//*[@name='{attribute}']")
+    tag.attrib['enable'] = str(random.randint(0, 1))
+    tag.attrib['area_length_a'] = str(random.uniform(populator_ranges['area_length_a_minimum'],
+                                                     populator_ranges['area_length_a_maximum']))
+    tag.attrib['area_length_b'] = str(random.uniform(populator_ranges['area_length_b_minimum'],
+                                                     populator_ranges['area_length_b_maximum']))
+    tag.attrib['object_spacing_xz_minimum'] = ' '.join(map(str, list(np.random.uniform(
+        low=populator_ranges['object_spacing_xz_minimum'],
+        high=populator_ranges['randomness_xz_maximum'], size=3))))
+    tag.attrib['minimum_scale'] = str(random.uniform(populator_ranges['minimum_scale_minimum'],
+                                                     populator_ranges['minimum_scale_maximum']))
+    tag.attrib['maximum_scale'] = str(random.uniform(populator_ranges['maximum_scale_minimum'],
+                                                     populator_ranges['maximum_scale_maximum']))
+    tag.attrib['diffuse_colour_multiplier'] = str(random.uniform(populator_ranges['diffuse_colour_multiplier_minimum'],
+                                                                 populator_ranges['diffuse_colour_multiplier_maximum']))
+    tag.attrib['luminosity_multiplier'] = str(random.uniform(populator_ranges['luminosity_multiplier_minimum'],
+                                                             populator_ranges['luminosity_multiplier_maximum']))
+    return tags_root
+
+def change_grass_population(tags_root: ET.Element, attribute="GrassOBJ") -> ET.Element:
+    with open(range_value_path, 'r') as range_file:
+        ranges = json.load(range_file)
+    populator_ranges = ranges['grass']
+
+    tag = tags_root.find(f".//*[@name='{attribute}']")
+    tag.attrib['enable'] = str(random.randint(0, 1))
+    tag.attrib['area_length_a'] = str(random.uniform(populator_ranges['area_length_a_minimum'],
+                                                     populator_ranges['area_length_a_maximum']))
+    tag.attrib['area_length_b'] = str(random.uniform(populator_ranges['area_length_b_minimum'],
+                                                     populator_ranges['area_length_b_maximum']))
+    tag.attrib['object_spacing_xz_minimum'] = ' '.join(map(str, list(np.random.uniform(
+        low=populator_ranges['object_spacing_xz_minimum'],
+        high=populator_ranges['randomness_xz_maximum'], size=3))))
+    tag.attrib['minimum_scale'] = str(random.uniform(populator_ranges['minimum_scale_minimum'],
+                                                     populator_ranges['minimum_scale_maximum']))
+    tag.attrib['maximum_scale'] = str(random.uniform(populator_ranges['maximum_scale_minimum'],
+                                                     populator_ranges['maximum_scale_maximum']))
+    tag.attrib['diffuse_colour_multiplier'] = str(random.uniform(populator_ranges['diffuse_colour_multiplier_minimum'],
+                                                     populator_ranges['diffuse_colour_multiplier_maximum']))
+    tag.attrib['luminosity_multiplier'] = str(random.uniform(populator_ranges['luminosity_multiplier_minimum'],
+                                                                 populator_ranges['luminosity_multiplier_maximum']))
+    return tags_root

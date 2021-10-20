d8e4ec321c37a9e0a8db08bb37bd9ecc8af69bfa
marcomameli1992 <m.mameli@pm.univpm.it>
2021-10-20 12:32:20 +0200

    Renaming the global values file

---
+++ basicComponent/global_value.py
@@ -0,0 +1,12 @@
+#%% seed variables
+use_seed: bool
+seed: int
+
+#%% base ranges folder path
+ranges_folder_path: str
+
+ranges_populator_path: str
+ranges_terrain_path: str
+ranges_shader_path: str
+ranges_light_path: str
+ranges_water_path: str

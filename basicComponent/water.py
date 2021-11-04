9214d168b3ac1ae2a9535113980a48b1d9f30f3e
marcomameli1992 <m.mameli@pm.univpm.it>
2021-11-04 09:51:25 +0100

    Update the center generation for the lake
    Now the center generation create a string with spaces between integer as terragen require.

--- basicComponent/water.py
+++ basicComponent/water.py
@@ -19,7 +19,7 @@ def change_fractal_water(tags_root: ET.Element, global_values: dict, attribute='
     tag.attrib['enable'] = str(random.randint(0, 1))  # at least one fractal power shader has to be active for the terrain generation
     level = str(random.randint(lake_ranges['water_level_minimum'], lake_ranges['water_level_maximum']))
     tag.attrib['water_level'] = level
-    tag.attrib['centre'] = str(random.randint(0, 1)) + level + str(random.randint(0, 1))
+    tag.attrib['centre'] = str(random.randint(0, 1)) + " " + level + " " + str(random.randint(0, 1))
     tag.attrib['max_radius'] = '10000'
     LOGGER.info(' change_fractal_water water_level value: ' + tag.attrib['water_level'])
     LOGGER.info(' change_fractal_water centre value: ' + tag.attrib['centre'])

d8b7fff732b63cd26fa469bf68a0a0aed45e1017
marcomameli1992 <m.mameli@pm.univpm.it>
2021-11-06 17:04:37 +0100

    Added populator object path changes
    This function changes the sep to os sep to use files from windows in linux/macOS

---
+++ basicComponent/populator_object.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+import logging
+import os
+
+#%% define logging
+LOGGER = logging.getLogger("POPULATOR_CHANGES")
+
+def change_populator_path(tags_root: ET.Element, populator_object='tgo_reader') -> ET.Element:
+    LOGGER.info(' called populator object paths')
+    for element in tags_root.findall(populator_object):
+        element.attrib['filename'] = element.attrib['filename'].replace('\\', os.sep)
+
+    return tags_root

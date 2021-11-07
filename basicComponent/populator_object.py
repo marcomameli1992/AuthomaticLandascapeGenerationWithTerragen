e900d3ee99c1c982cb3b3005afe61c7ee2561c9c
marcomameli1992 <m.mameli@pm.univpm.it>
2021-11-07 14:55:24 +0100

    insert print

--- basicComponent/populator_object.py
+++ basicComponent/populator_object.py
@@ -8,6 +8,7 @@ LOGGER = logging.getLogger("POPULATOR_CHANGES")
 def change_populator_path(tags_root: ET.Element, populator_object='tgo_reader') -> ET.Element:
     LOGGER.info(' called populator object paths')
     for element in tags_root.findall(populator_object):
+        print(element.attrib['filename'])
         element.attrib['filename'] = element.attrib['filename'].replace('\\', os.sep)
 
     return tags_root

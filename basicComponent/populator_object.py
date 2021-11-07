80470e3cbd75587a7894dd251329905d41f201d1
marcomameli1992 <m.mameli@pm.univpm.it>
2021-11-07 15:17:30 +0100

    Changed the populator function

--- basicComponent/populator_object.py
+++ basicComponent/populator_object.py
@@ -10,6 +10,8 @@ def change_populator_path(tags_root: ET.Element, populator_tag='populator_v4', p
     for element in tags_root.findall(populator_tag):
         for reader in element.findall(populator_reader):
             print('Old path', reader.attrib['filename'])
+            new_path = reader.attrib['filename'].replace('\\', os.sep)
+            print('New path', new_path)
             reader.attrib['filename'] = reader.attrib['filename'].replace('\\', os.sep)
 
     return tags_root
